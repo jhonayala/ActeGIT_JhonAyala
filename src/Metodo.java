4fead790bb3534d02915581efeccc1fb19031bdb
Jhon <a_jhondeivis.ayala.braithwite@iespablopicasso.es>
2021-11-30 12:10:05 +0100

    creo un metodo para añadir 20 numeros aleatorios a un array.

---
+++ src/Metodo.java
@@ -0,0 +1,13 @@
+
+public class Metodo {
+
+	public static void main(String[] args) {
+		int [] array=new int[20];
+		for (int i = 0; i < array.length; i++) {
+			int m=(int)(Math.random()*10+1);
+			array[i]=m;
+			
+		}
+	}
+
+}
