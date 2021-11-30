afd4ab28a7c5f86c9bf2703f957bec87e490316c
Jhon <a_jhondeivis.ayala.braithwite@iespablopicasso.es>
2021-11-30 13:04:37 +0100

    cambio el tamaño del array a 30.

--- src/Metodo.java
+++ src/Metodo.java
@@ -2,7 +2,7 @@
 public class Metodo {
 
 	public static void main(String[] args) {
-		int [] array=new int[20];
+		int [] array=new int[30];
 		for (int i = 0; i < array.length; i++) {
 			int m=(int)(Math.random()*10+1);
 			array[i]=m;
